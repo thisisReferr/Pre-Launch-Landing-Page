ca7bad07b3331743fc6dcde3d1c4f89cc465e38d
SykiK <xo.sykik@gmail.com>
2023-08-05 19:06:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+'use strict'
+
+const menuToggle = document.querySelector('.menu-toggle');
+const bxMenu = document.querySelector('.bx-menu');
+const bxX = document.querySelector('.bx-x');
+
+const navBar = document.querySelector('.navbar');
