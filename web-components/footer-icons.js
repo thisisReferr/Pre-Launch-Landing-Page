8c41c869ccf6570fc48d423c090a5a21981df088
AyanavaKarmakar <karmakarayanava2021@gmail.com>
2023-08-05 17:18:16 +0530

    chore: format code

--- web-components/footer-icons.js
+++ web-components/footer-icons.js
@@ -1,33 +1,35 @@
 class FooterIcons extends HTMLElement {
-    connectedCallback() {
-        // Retrieve the icons data from an attribute
-        let iconsData = JSON.parse(this.getAttribute('icons'));
-        if (!iconsData) {
-            iconsData = [
-                {
-                    alt: "email",
-                    src: "./public/email.svg"
-                },
-                {
-                    alt: "linkedin",
-                    src: "./public/linkedin.svg"
-                },
-            ];
-        }
+  connectedCallback() {
+    // Retrieve the icons data from an attribute
+    let iconsData = JSON.parse(this.getAttribute("icons"));
+    if (!iconsData) {
+      iconsData = [
+        {
+          alt: "email",
+          src: "./public/email.svg",
+        },
+        {
+          alt: "linkedin",
+          src: "./public/linkedin.svg",
+        },
+      ];
+    }
 
-        // Generate the markup for the icons
-        const iconsMarkup = iconsData
-            .map(icon => `<a href="#"><img alt="${icon.alt}" src="${icon.src}" /></a>`)
-            .join('');
+    // Generate the markup for the icons
+    const iconsMarkup = iconsData
+      .map(
+        (icon) => `<a href="#"><img alt="${icon.alt}" src="${icon.src}" /></a>`,
+      )
+      .join("");
 
-        // Set the innerHTML of the component
-        this.innerHTML = `
+    // Set the innerHTML of the component
+    this.innerHTML = `
             <div class="footer_right">
                 ${iconsMarkup}
             </div>
         `;
-    }
+  }
 }
 
 // Define the custom element
-customElements.define('footer-icons', FooterIcons);
+customElements.define("footer-icons", FooterIcons);
--- web-components/footer-links.js
+++ web-components/footer-links.js
@@ -1,33 +1,33 @@
 class FooterLinks extends HTMLElement {
-    connectedCallback() {
-      // Retrieve the links data from an attribute
-      let linksData = JSON.parse(this.getAttribute('links'));
-      if (!linksData){
-        linksData =[
-            {
-                name:"Terms & Conditions",
-                link:"#"
-            },
-            {
-                name:"Privacy",
-                link:"#"
-            }
-        ]
-      }
-  
-      // Generate the markup for the links
-      const linksMarkup = linksData
-        .map(link => `<a href="${link.link}"><p>${link.name}</p></a>`)
-        .join('');
-  
-      // Set the innerHTML of the component
-      this.innerHTML = `
+  connectedCallback() {
+    // Retrieve the links data from an attribute
+    let linksData = JSON.parse(this.getAttribute("links"));
+    if (!linksData) {
+      linksData = [
+        {
+          name: "Terms & Conditions",
+          link: "#",
+        },
+        {
+          name: "Privacy",
+          link: "#",
+        },
+      ];
+    }
+
+    // Generate the markup for the links
+    const linksMarkup = linksData
+      .map((link) => `<a href="${link.link}"><p>${link.name}</p></a>`)
+      .join("");
+
+    // Set the innerHTML of the component
+    this.innerHTML = `
         <div class="footer_left">
           ${linksMarkup}
         </div>
       `;
-    }
   }
-  
-  // Define the custom element
-  customElements.define('footer-links', FooterLinks);
+}
+
+// Define the custom element
+customElements.define("footer-links", FooterLinks);
