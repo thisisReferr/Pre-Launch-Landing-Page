32aca5b86db58b704f667aeaca7f9168184f08dc
Abhishek.Diwate <abhishek.diwate@ivycomptech.com>
2023-08-06 01:25:12 +0530

    prettire-format

--- index.js
+++ index.js
@@ -1,50 +1,48 @@
-const emailInput = document.getElementById('emailInput');
-const getNotified = document.getElementById("getNotified"); 
+const emailInput = document.getElementById("emailInput");
+const getNotified = document.getElementById("getNotified");
 function emailExists(email) {
-    // Should make request to server if email already exists
-    const existingEmails = ['example@example.com'];
-    return existingEmails.includes(email);
+  // Should make request to server if email already exists
+  const existingEmails = ["example@example.com"];
+  return existingEmails.includes(email);
 }
 
-const getNotifiedEventHandler = (event)=>{
-    const email = emailInput.value.trim();
-    if (emailExists(email)) {
-        emailInput.classList.remove(['active', 'inactive', 'valid', 'invalid']);
-        emailInput.classList.add('email-exists');
-    } else {
-        emailInput.classList.remove('email-exists');
-
-    }
-}
+const getNotifiedEventHandler = (event) => {
+  const email = emailInput.value.trim();
+  if (emailExists(email)) {
+    emailInput.classList.remove(["active", "inactive", "valid", "invalid"]);
+    emailInput.classList.add("email-exists");
+  } else {
+    emailInput.classList.remove("email-exists");
+  }
+};
 
 function handleInputEvent() {
-    const email = emailInput.value.trim();
+  const email = emailInput.value.trim();
 
-    if(emailInput.classList.contains('active')&& email) {
-        emailInput.classList.remove('active');
-    }
+  if (emailInput.classList.contains("active") && email) {
+    emailInput.classList.remove("active");
+  }
 
-    if (email === '') {
-        emailInput.classList.remove('valid', 'invalid', 'email-exists');
-    } else if (emailInput.validity.valid) {
-        emailInput.classList.add('valid');
-        emailInput.classList.remove('invalid');
-    } else {
-        emailInput.classList.remove('valid');
-        emailInput.classList.add('invalid');
-    }
-
+  if (email === "") {
+    emailInput.classList.remove("valid", "invalid", "email-exists");
+  } else if (emailInput.validity.valid) {
+    emailInput.classList.add("valid");
+    emailInput.classList.remove("invalid");
+  } else {
+    emailInput.classList.remove("valid");
+    emailInput.classList.add("invalid");
+  }
 }
 
 function handleFocusEvent() {
-    emailInput.classList.add('active');
+  emailInput.classList.add("active");
 }
 
 function handleBlurEvent() {
-    emailInput.classList.remove('active');
+  emailInput.classList.remove("active");
 }
 
-emailInput.addEventListener('input', handleInputEvent);
-emailInput.addEventListener('focus', handleFocusEvent);
-emailInput.addEventListener('blur', handleBlurEvent);
-getNotified.addEventListener('click',getNotifiedEventHandler)
+emailInput.addEventListener("input", handleInputEvent);
+emailInput.addEventListener("focus", handleFocusEvent);
+emailInput.addEventListener("blur", handleBlurEvent);
+getNotified.addEventListener("click", getNotifiedEventHandler);
--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-importScripts('https://cdn.jsdelivr.net/npm/workbox-cdn/workbox/workbox-sw.js')
+importScripts("https://cdn.jsdelivr.net/npm/workbox-cdn/workbox/workbox-sw.js");
 
 workbox.routing.registerRoute(
-    ({request})=>request.destination === 'image',
-    new workbox.strategies.CacheFirst()
-)
+  ({ request }) => request.destination === "image",
+  new workbox.strategies.CacheFirst(),
+);
