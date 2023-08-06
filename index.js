e8332bdd8c6c3e67a175a4f60232a1f3bdeb7e42
Abhishek.Diwate <abhishek.diwate@ivycomptech.com>
2023-08-06 11:36:31 +0530

    improved-js

--- index.js
+++ index.js
@@ -1,48 +1,56 @@
 const emailInput = document.getElementById("emailInput");
-const getNotified = document.getElementById("getNotified");
+const notifiedForm = document.getElementById("notifiedForm");
+
+function isValidEmail(email) {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(email);
+}
+
 function emailExists(email) {
-  // Should make request to server if email already exists
-  const existingEmails = ["example@example.com"];
-  return existingEmails.includes(email);
+    const existingEmails = ["example@example.com"];
+    return existingEmails.includes(email);
 }
 
 const getNotifiedEventHandler = (event) => {
-  const email = emailInput.value.trim();
-  if (emailExists(email)) {
-    emailInput.classList.remove(["active", "inactive", "valid", "invalid"]);
-    emailInput.classList.add("email-exists");
-  } else {
-    emailInput.classList.remove("email-exists");
-  }
+    event.preventDefault();
+    console.log("event triggered")
+    const { value } = emailInput;
+    const email = value.trim();
+
+    if (!isValidEmail(email)) {
+        emailInput.classList.add("invalid");
+        return;
+    }
+
+    if (emailExists(email)) {
+        emailInput.classList.replace("valid", "email-exists");
+    }
 };
 
 function handleInputEvent() {
-  const email = emailInput.value.trim();
+    const { value } = emailInput;
+    const email = value.trim();
 
-  if (emailInput.classList.contains("active") && email) {
-    emailInput.classList.remove("active");
-  }
+    emailInput.classList.remove("valid", "invalid", "email-exists" , "active");
 
-  if (email === "") {
-    emailInput.classList.remove("valid", "invalid", "email-exists");
-  } else if (emailInput.validity.valid) {
-    emailInput.classList.add("valid");
-    emailInput.classList.remove("invalid");
-  } else {
-    emailInput.classList.remove("valid");
-    emailInput.classList.add("invalid");
-  }
+    if (email === "") return;
+
+    if (emailInput.validity.valid) {
+        emailInput.classList.add("valid");
+    } else {
+        emailInput.classList.add("invalid");
+    }
 }
 
 function handleFocusEvent() {
-  emailInput.classList.add("active");
+    emailInput.classList.add("active");
 }
 
 function handleBlurEvent() {
-  emailInput.classList.remove("active");
+    emailInput.classList.remove("active");
 }
 
 emailInput.addEventListener("input", handleInputEvent);
 emailInput.addEventListener("focus", handleFocusEvent);
 emailInput.addEventListener("blur", handleBlurEvent);
-getNotified.addEventListener("click", getNotifiedEventHandler);
+notifiedForm.addEventListener("submit", getNotifiedEventHandler);
