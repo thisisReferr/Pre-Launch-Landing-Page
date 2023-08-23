c2d22b0a29b0817388677d2347d0119a8881c333
Abhishek.Diwate <abhishek.diwate@ivycomptech.com>
2023-08-23 10:27:09 +0530

    chore:T&C

---
+++ web-components/privacy-component/privacy-points.js
@@ -0,0 +1,70 @@
+export const privacyPoints = [
+  {
+    title: "1. Collection of Personal Information:",
+    description:
+      "We collect personal information that you provide directly to us when you use our services. \nPersonal information including but not limited to:",
+    items: [
+      "Personal details: Name, contact information, and other relevant identifiers.",
+      "Professional information: Resumes, work history, skills, qualifications, certifications, and other relevant employment-related details.",
+      "Technical assessment scores: LeetCode, HackerRank, and similar technical assessment scores and performance data.",
+    ],
+  },
+  {
+    title: "2. Use of Personal Information:",
+    description:
+      "We use the collected personal information for various purposes, including but not limited to:",
+    items: [
+      "Providing and improving our services to you",
+      "Responding to your inquiries and requests",
+      "Sending you relevant updates and notifications",
+      "Analyzing and enhancing the effectiveness of our services",
+      "Complying with legal obligations and protecting our rights",
+    ],
+  },
+  {
+    title: "3. Sharing of Personal Information:",
+    description:
+      "We may share your personal information with third parties in the following situations:",
+    items: [
+      "With your explicit consent",
+      "To comply with legal obligations",
+      "With service providers who assist us in delivering our services",
+      "In connection with a merger, acquisition, or sale of assets, where your data may be transferred as part of the transaction",
+    ],
+  },
+  {
+    title: "4. Data Security:",
+    description:
+      "We implement and maintain reasonable security measures to protect your personal information from unauthorized access, disclosure, \nor alteration. However, no data transmission over the internet can be guaranteed to be entirely secure, so you should take precautions to \nprotect your information as well.",
+    items: [],
+  },
+  {
+    title: "5. Your Rights:",
+    description:
+      "You have certain rights regarding your personal data, which may include:",
+    items: [
+      "Access: You can request a copy of the personal data we hold about you.",
+      "Correction: You can ask us to correct any inaccurate or incomplete personal data.",
+      "Deletion: You can request the deletion of your personal data, subject to legal obligations.",
+      "Withdrawal of Consent: You can withdraw your consent for processing your data at any time.",
+    ],
+  },
+  {
+    title: "6. International Data Transfers:",
+    description:
+      "Your personal information may be transferred to, and processed in, countries other than the one in which you reside. We will ensure that \nsuch transfers are in compliance with applicable data protection laws and that appropriate safeguards are in place.",
+    items: [],
+  },
+  {
+    title: "7. Changes to the Privacy Policy:",
+    description:
+      "We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or \nregulatory reasons. We will provide notice of any significant changes.",
+    items: [],
+  },
+  {
+    title: "8. Contact Us:",
+    description:
+      "If you have any questions, concerns, or requests related to this Privacy Policy, please contact our Privacy Team at \nprivacy@teamreferr.com",
+    items: [],
+  },
+];
